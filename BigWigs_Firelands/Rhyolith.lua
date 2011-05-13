8d29d34eacddc5f2a031c03c4e02a391ce5503da
hiski <hiskii@gmail.com>
2011-05-13 19:08:27 +0300

    toka

--- BigWigs_Firelands/Rhyolith.lua
+++ BigWigs_Firelands/Rhyolith.lua
@@ -8,7 +8,7 @@ if not mod then return end
 mod:RegisterEnableMob(52558)
 
 --------------------------------------------------------------------------------
--- Locals
+-- Locals   
 --
 
 local phase = 1
